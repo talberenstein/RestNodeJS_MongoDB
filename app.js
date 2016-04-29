ae3a25f9a10fdb44bd2614762e35d2d71a24c02a
Tal Zeev Torres <talzeevt@MacBook-de-Tal.local>
2016-04-29 02:37:20 -0300

    bd

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ var options = {
   pass: process.env.OPENSHIFT_MONGODB_DB_PASSWORD
 }
 // Connection to DB
-mongoose.connect('mongodb://'+process.env.OPENSHIFT_MONGODB_DB_HOST+':'+process.env.OPENSHIFT_MONGODB_DB_PORT+'/'+'eventSchema',options);
+mongoose.connect('mongodb://'+process.env.OPENSHIFT_MONGODB_DB_HOST+':'+process.env.OPENSHIFT_MONGODB_DB_PORT+'/'+'starwars',options);
 
 // Middlewares
 app.use(bodyParser.urlencoded({ extended: false }));
